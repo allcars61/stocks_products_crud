65f8cff7e218ba14602d3de6bd468169b0f95c9b
Aleksandr Tolkanev <allcars61@gmail.com>
2023-05-26 11:54:47 +0300

    Update models.py

--- logistic/models.py
+++ logistic/models.py
@@ -1,35 +1,28 @@
-from django.core.validators import MinValueValidator
 from django.db import models
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=60, unique=True)
-    description = models.TextField(null=True, blank=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
 
 
 class Stock(models.Model):
-    address = models.CharField(max_length=200, unique=True)
-    products = models.ManyToManyField(
-        Product,
-        through="StockProduct",
-        related_name="stocks",
-    )
+    location = models.CharField(max_length=255)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.location} - {self.date}"
 
 
 class StockProduct(models.Model):
-    stock = models.ForeignKey(
-        Stock,
-        on_delete=models.CASCADE,
-        related_name="positions",
-    )
-    product = models.ForeignKey(
-        Product,
-        on_delete=models.CASCADE,
-        related_name="positions",
-    )
-    quantity = models.PositiveIntegerField(default=1)
-    price = models.DecimalField(
-        max_digits=18,
-        decimal_places=2,
-        validators=[MinValueValidator(0)],
-    )
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE, related_name="positions")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.stock} - {self.product}"
